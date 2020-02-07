4bdf7c4023978f319daed4a14c37c469b6594508
Manuel <njester5030@gmail.com>
2020-02-07 12:48:14 +0100

    one param support added

--- magic.sh
+++ magic.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
 function magic {
-  for f in $2/*\ *; do mv "$f" "${f// /_}"; done
+  if [ $# -eq 2 ]
+  then 
+    for f in $2/*\ *; do mv "$f" "${f// /_}"; done
+  else
+    for f in $1/*\ *; do mv "$f" "${f// /_}"; done
+  fi
   exit 0
 }
